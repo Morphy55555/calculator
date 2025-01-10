ca819941632fd820708a0f05ef9adfc6d611e330
Morphy55555 <learningthings1996@gmail.com>
2025-01-10 14:25:22 +0700

    Implemented display, calculation and reset functionality

--- calculator.js
+++ calculator.js
@@ -1,5 +1,13 @@
+const display = document.querySelector('.display');
+const btn = document.querySelectorAll('button'); 
+
+firstNumber = '';
+secondNumber = '';
+operator = '';
+
+
 function addition(num1, num2) {
-    return num1 + num2;
+    return +num1 + +num2;
 }
 
 function subtract(num1, num2) {
@@ -14,11 +22,67 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-
-firstNumber = null;
-SecondNumber = null;
-operator = null;
-
-function operate(firstN,op,secondN) {
-    return op(firstN, secondN)
+function resetCalc() {
+    display.textContent = '';
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
 }
+
+
+function operate(firstN, opp, secondN) {
+    const operations = {
+        '+': addition,
+        '-': subtract,
+        '*': multiply,
+        '/': divide
+    };
+
+    const operationFunction = operations[opp]; 
+    if (operationFunction) {
+        console.log(operationFunction(firstN, secondN))
+        return operationFunction(firstN, secondN); 
+    
+}
+}
+
+
+function displayOnBtnClick(){
+btn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        clickedButton = e.target.id;
+        const displayText = document.createElement('span');
+        displayText.textContent = clickedButton;
+        display.appendChild(displayText);
+
+        if (clickedButton === '-' || 
+            clickedButton === '+' || 
+            clickedButton === '*' || 
+            clickedButton === '/') {
+            operator += clickedButton;
+
+        } else if (operator !== '' && clickedButton !== '=' && clickedButton !== 'AC') {
+            secondNumber += clickedButton;
+        } else  if (clickedButton !== '=' && clickedButton !== 'AC') {
+            firstNumber += clickedButton;
+        } else if (clickedButton === 'AC') {
+            resetCalc();
+        };
+
+        if (clickedButton === '=') {
+            display.textContent = ''; 
+            const result = operate(firstNumber,operator,secondNumber);
+            const displayText = document.createElement('span');
+            displayText.textContent = result;
+            display.appendChild(displayText);
+        }
+        console.log(clickedButton);
+        
+    })
+});
+};
+
+
+
+
+displayOnBtnClick();
